6c77abe8f23e053be69b3dcc5e42e5e0d3d143b5
james <devnull@localhost>
2002-09-04 10:24:01 -0700

    Added callback + setcallback function to detect hangups Added channel_status function

--- lib/Asterisk.pm
+++ lib/Asterisk.pm
@@ -2,7 +2,7 @@ package Asterisk;
 
 require 5.004;
 
-$VERSION = '0.02';
+$VERSION = '0.03';
 
 sub version { $VERSION; }
 
--- lib/Asterisk/AGI.pm
+++ lib/Asterisk/AGI.pm
@@ -36,6 +36,8 @@ asterisk open source pbx via AGI (asterisk gateway interface)
 sub new {
 	my ($class, %args) = @_;
 	my $self = {};
+	$self->{'callback'} = undef;
+	$self->{'status'} = undef;
 	bless $self, ref $class || $class;
 	return $self;
 }
@@ -69,6 +71,22 @@ sub ReadParse {
 	return %input;
 }
 
+sub setcallback {
+	my ($self, $function) = @_;
+
+	if (defined($function) && ref($function) eq 'CODE') {
+		$self->{'callback'} = $function;
+	} 
+}
+
+sub callback {
+	my ($self, $result) = @_;
+
+	if (defined($self->{'callback'}) && ref($self->{'callback'}) eq 'CODE') {
+		&{$self->{'callback'}}($result);
+	}
+}
+
 sub execute {
 	my ($self, $command) = @_;
 
@@ -94,9 +112,9 @@ sub _execcommand {
 sub _readresponse {
 	my ($self, $fh) = @_;
 
-	my $response = '';
+	my $response = undef;
 	$fh = STDIN if (!$fh);
-	$response = <$fh>;
+	$response = <$fh> || return '200 result=-1 (noresponse)';
 	chomp($response);
 	return $response;
 }
@@ -104,73 +122,121 @@ sub _readresponse {
 sub _checkresult {
 	my ($self, $response) = @_;
 
-
-	return -1 if (!defined($response));
-	my $result = -1;
+	return undef if (!defined($response));
+	my $result = undef;
 
 	if ($response =~ /^200/) {
 		if ($response =~ /result=(-?\d+)/) {
 			$result = $1;
 		}
+	} elsif ($response =~ /\(noresponse\)/) {
+		$self->_status('noresponse');
 	} else {
 		print STDERR "Unexpected result '$response'\n" if ($DEBUG);
 	}
+	print STDERR "_checkresult($response) = $result\n" if ($DEBUG>3);
 
 	return $result;				
+}
+
+sub _status {
+	my ($self, $status) = @_;
+
+	if (defined($status)) {
+		$self->{'status'} = $status;
+	} else {
+		return $self->{'status'};
+	}
 }
 
 sub stream_file {
 	my ($self, $filename, $digits) = @_;
 
+	my $ret = 0;
+
 	$digits = '""' if (!defined($digits));
 
 	return -1 if (!defined($filename));
-	return $self->execute("STREAM FILE $filename $digits");
+	$ret =  $self->execute("STREAM FILE $filename $digits");
+
+	$self->callback($ret) if ($ret == -1);
+
+	return $ret;
 }
 
 sub send_text {
 	my ($self, $text) = @_;
 
-	return 0 if (!defined($text));
-	return $self->execute("SEND TEXT \"$text\"");
+	my $ret = 0;
+
+	return $ret if (!defined($text));
+	$ret = $self->execute("SEND TEXT \"$text\"");
+	$self->callback($ret) if ($ret == -1);
+
+	return $ret;
 }
 
 sub send_image {
 	my ($self, $image) = @_;
+
+	my $ret = 0;
 	return -1 if (!defined($image));
 
-	return $self->execute("SEND IMAGE $image");
+	$ret = $self->execute("SEND IMAGE $image");
+	$self->callback($ret) if ($ret == -1);
+
+	return $ret;
 }
 
 sub say_number {
 	my ($self, $number, $digits) = @_;
 
+	my $ret = 0;
+
 	$digits = '""' if (!defined($digits));
 
 	return -1 if (!defined($number));
-	return $self->execute("SAY NUMBER $number $digits");
+	$ret = $self->execute("SAY NUMBER $number $digits");
+
+	$self->callback($ret) if ($ret == -1);
+
+	return $ret;
 }
 
 sub say_digits {
         my ($self, $number, $digits) = @_;
 
+	my $ret = 0;
 	$digits = '""' if (!defined($digits));
 
 	return -1 if (!defined($number));
-	return $self->execute("SAY DIGITS $number $digits");
+	$ret = $self->execute("SAY DIGITS $number $digits");
+	$self->callback($ret) if ($ret == -1);
+
+	return $ret;
 }
 
 sub answer {
 	my ($self) = @_;
 
-	return $self->execute('ANSWER');
+	my $ret = 0;
+	$ret = $self->execute('ANSWER');
+	$self->callback($ret) if ($ret == -1);
+
+	return $ret;
+
 }
 
 sub get_data {
 	my ($self, $filename, $timeout, $maxdigits) = @_;
 
+	my $ret = undef;
+
 	return -1 if (!defined($filename));
-	return $self->execute("GET DATA $filename $timeout $maxdigits");
+	$ret = $self->execute("GET DATA $filename $timeout $maxdigits");
+	$self->callback($ret) if ($ret == -1);
+
+	return $ret;
 }
 
 sub set_callerid {
@@ -204,9 +270,14 @@ sub set_priority {
 sub receive_char {
 	my ($self, $timeout) = @_;
 
+	my $ret = 0;
 #wait forever if timeout is not set. is this the prefered default?
 	$timeout = 0 if (!defined($timeout));
-	return $self->execute("RECEIVE CHAR $timeout");
+	$ret = $self->execute("RECEIVE CHAR $timeout");
+	$self->callback($ret) if ($ret == -1);
+
+	return $ret;
+
 }
 
 sub tdd_mode {
@@ -220,16 +291,27 @@ sub tdd_mode {
 sub wait_for_digit {
 	my ($self, $timeout) = @_;
 
+	my $ret = 0;
 	$timeout = -1 if (!defined($timeout));
-	return $self->execute("WAIT FOR DIGIT $timeout");
+	$ret = $self->execute("WAIT FOR DIGIT $timeout");
+
+	$self->callback($ret) if ($ret == -1);
+
+	return $ret;
 }
 
 sub record_file {
 	my ($self, $filename, $format, $digits, $timeout, $beep) = @_;
 
+	my $ret = 0;
+
 	return -1 if (!defined($filename));
 	$digits = '""' if (!defined($digits));
-	return $self->execute("RECORD FILE $filename $format $digits $timeout");
+	$ret = $self->execute("RECORD FILE $filename $format $digits $timeout");
+
+	$self->callback($ret) if ($ret == -1);
+
+	return $ret;
 }
 
 sub set_autohangup {
@@ -250,6 +332,12 @@ sub exec {
 	return -1 if (!defined($app));
 	$options = '""' if (!defined($options));
 	return $self->execute("EXEC $app $options");
+}
+
+sub channel_status {
+	my ($self) = @_;
+
+	return $self->execute("CHANNEL STATUS");
 }
 
 
